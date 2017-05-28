690caa1439ce42b29a1072a779756da60bc8ad3e
Zuber <zuberahmed99@gmail.com>
2017-05-28 20:14:17 +0530

    added selenium

--- src/data/extractor/parsehtml.py
+++ src/data/extractor/parsehtml.py
@@ -1,12 +1,16 @@
 from urllib import urlopen
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 def getBeautifulSoupObject(link):
     html = urlopen(link)
     return BeautifulSoup(html)
 
+def getDynamicContent(link):
+    driver = webdriver.PhantomJS()
+    driver.get(link)
 
 
 link = "https://www.dnainfo.com/chicago/2017-chicago-murders"
 
-print getBeautifulSoupObject(link)
+getDynamicContent(link)
