534770cf0cc25c7aa0350570bc7a39d6239c7e05
Chankey Pathak <chankey007@gmail.com>
2017-05-29 13:21:54 +0530

    Add logging, requests and handle exceptions

--- src/data/extractor/parsehtml.py
+++ src/data/extractor/parsehtml.py
@@ -1,15 +1,25 @@
-from urllib import urlopen
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+import logging
+logging.basicConfig(filename='scrape.log',level=20)
+logging.info('Initialized logger')
+
 def getBeautifulSoupObject(link):
-    html = urlopen(link)
+    html = requests.get(link)
     return BeautifulSoup(html)
 
 def getDynamicContent(link):
-    driver = webdriver.PhantomJS()
+    try:
+        driver = webdriver.PhantomJS()
+        logging.info("Webdriver initiated {0}".format(driver))
+    except Exception as exp:
+        logging.fatal("Webdriver exception: {exception}".format(exception=exp))
+        raise exp
     driver.get(link)
-
+    #REMOVELATER: test if we are able to get the title
+    print driver.title
 
 link = "https://www.dnainfo.com/chicago/2017-chicago-murders"
 
