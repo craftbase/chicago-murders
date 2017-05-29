d70a1e8ec416a979b14596bf6b603b9d1b1faf09
Chankey Pathak <chankey007@gmail.com>
2017-05-29 16:41:53 +0530

    Get information of each victim

--- src/data/extractor/parsehtml.py
+++ src/data/extractor/parsehtml.py
@@ -1,12 +1,15 @@
+import logging
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-import logging
-logging.basicConfig(filename='scrape.log',level=20)
+logging.basicConfig(filename='scrape.log', level=20)
 logging.info('Initialized logger')
+
 
 def get_soup_object(html):
     return BeautifulSoup(html)
+
 
 def get_dynamic_content(link):
     try:
@@ -18,12 +21,24 @@ def get_dynamic_content(link):
     driver.get(link)
     html = driver.page_source
     soup = get_soup_object(html)
-    #Dump the page source to log for debugging purpose, this will work only if level is set to 10 in line number 5
+    # Dump the page source to log for debugging purpose, this will work only if level is set to 10 in line number 5
     logging.debug(soup.prettify())
     parsedata(soup)
 
+
 def parsedata(soup):
-    pass
+    victims = soup.find_all("div", {"class": "homicide"})
+    for victim in victims:
+        date = victim.find("div", {"class": "date"}).get_text().strip()
+        name = victim.find("h2", {"class": "name"}).get_text().strip()
+        age = victim.find("div", {"class": "age"}).contents[1].strip()
+        race = victim.find("div", {"class": "race"}).contents[1].strip()
+        cause = victim.find("div", {"class": "cause"}).contents[1].strip()
+        neighbourhood = victim.find("div", {"class": "neighborhood"}).contents[1].strip()
+        time = victim.find("span", {"class": "murder_time"}).get_text().strip()
+        addr = victim.find("div", {"class": "address"}).contents[2].strip()
+        print "Name " + name + " Age " + age + " Race " + race + " Cause " + cause + " Neigbourhood " + neighbourhood + " Time " + time + " Address " + addr
+
 
 link = "https://www.dnainfo.com/chicago/2017-chicago-murders"
 
