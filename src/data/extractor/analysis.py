bb601afc6047ae286718d1dfe97de005e7cef153
Zuber <zuberahmed99@gmail.com>
2017-06-18 02:12:44 +0530

    added method to replace non numeric age with numeric ones

--- src/data/extractor/analysis.py
+++ src/data/extractor/analysis.py
@@ -16,13 +16,20 @@ def get_parameters(df):
 def remove_non_numeric_age(df):
     df['age'].fillna(method="ffill")
     mask = df['age'].str.contains("years")
-
+    non_numeric_age = df[mask]
+    df.loc[df['age'].str.contains("years"),'age'] = extract_numeric_age(df['age'].str)
 
 def clean_data(df):
     remove_non_numeric_age(df)
     return df
 
-new_df = clean_data(df)
-print new_df['age'].describe()
+def extract_numeric_age(string):
+    #for char in string:
+    sub_strings = string.split()
+
+    return sub_strings[0];
+
+#new_df = clean_data(df)
+#print new_df['age'].describe()
 
 #get_parameters(df)
