b1d312eb55616aad232fedb54dc4f0ac7c8188ce
Zuber <zuberahmed99@gmail.com>
2017-06-22 20:06:08 +0530

    added median values to missing values of age

--- src/data/extractor/analysis.py
+++ src/data/extractor/analysis.py
@@ -14,13 +14,15 @@ def get_parameters(df):
     return age_race
 
 def remove_non_numeric_age(df):
-    df['age'].fillna(method="ffill")
+
     mask = df['age'].str.contains("years")
     non_numeric_age = df[mask]
-    df.loc[df['age'].str.contains("years"),'age'] = extract_numeric_age(df['age'].str)
+    df = df[df['age'].str.contains("year"),'age'].apply(extract_numeric_age)
+
 
 def clean_data(df):
     remove_non_numeric_age(df)
+    df['age'].fillna(df['age'].median(),inplace=True)
     return df
 
 def extract_numeric_age(string):
