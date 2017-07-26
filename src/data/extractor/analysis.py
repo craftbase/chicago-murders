a35008f1e31eed908d6d69f43691c656d316d3f8
Zuber <zuberahmed99@gmail.com>
2017-07-26 23:06:44 +0530

    updated the method to scale time

--- src/data/extractor/analysis.py
+++ src/data/extractor/analysis.py
@@ -15,12 +15,13 @@ def get_parameters(df):
     return age_race
 
 def remove_non_numeric_age(df):
-    df.dropna(axis=0,inplace=True)
+
     #df = df(df['age'].str.contains("year")).loc['age'].apply(extract_numeric_age)
     df['age'] = df['age'].apply(extract_numeric_age)
     return df
 
 def clean_data(df):
+    df.dropna(axis=0, inplace=True)
     df = remove_non_numeric_age(df)
     df['time'] = df['time'].apply(scale_time)
     #df['age'].fillna(df['age'].median(),inplace=True)
@@ -37,18 +38,38 @@ def extract_numeric_age(string):
 
 def scale_time(string):
     scaled_time = -100
-    if ":" in string:
-        sub_strings = string.split()
-        time = sub_strings[0]
-        am_pm = sub_strings[1]
-        hour = time.split(":")[0]
-        minute = time.split(":")[1]
-        hour = int(hour)
-        minute = int(minute)
-        if am_pm[0] == 'p' and hour != 12:
-            hour = 12 + hour
-        scaled_time = hour*60 + minute
+    am_pm = "something"
+    try:
+        if ":" in string:
+            sub_strings = string.split()
+            time = sub_strings[0]
+            if len(string)>5:
+                am_pm = sub_strings[1]
+            else:
+                am_pm = "a"
+
+            hour = time.split(":")[0]
+            minute = time.split(":")[1]
+            hour = int(hour)
+            minute = int(minute)
+            if am_pm[0] == 'p' and hour != 12:
+                hour = 12 + hour
+            scaled_time = hour*60 + minute
+        else:
+            sub_strings = string.split()
+            time = sub_strings[0]
+            am_pm = sub_strings[1]
+            hour = int(time)
+            if am_pm[0] == 'p' and hour != 12:
+                hour = 12 + hour
+            scaled_time = hour*60
+    except IndexError:
+        print(string)
+        print(am_pm)
     return scaled_time
+
+
+
 
 def has_string(string):
     return bool(re.search(r'\D', string))
