3153562642506bc34acaeba9bae09ab2c1bfe4b5
Zuber <zuberahmed99@gmail.com>
2017-08-02 00:30:53 +0530

    added random forest

--- src/data/extractor/analysis.py
+++ src/data/extractor/analysis.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-
+from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv("victim_info_2012_2017.csv")
 
@@ -27,4 +27,21 @@ def predict_logistic_regression(df):
     print(classification_report(y_test, predictions))
 
 
-predict_logistic_regression(df)
+def predict_random_forest(df):
+    df = preprocessing.clean_data(df)
+    df = preprocessing.get_dummies(df)
+    # get X variables
+    X = df[['age', 'time', 'Cause__Stabbing', 'Cause__Assault', 'Cause__Auto Crash', 'Cause__Other', 'Cause__Shooting',
+            'Cause__Strangulation']]
+    y = df['Race__White']
+    print "splitting test and training data"
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=101, test_size=0.3)
+    rf = RandomForestClassifier(n_estimators=700)
+    print "Fitting the training data into the random forest"
+    rf.fit(X_train, y_train)
+    print "Predicting values of test data"
+    predictions = rf.predict(X_test)
+    print "Printing classification report"
+    print(classification_report(y_test, predictions))
+
+predict_random_forest(df)
