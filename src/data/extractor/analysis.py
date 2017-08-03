6110b8979d36e9dd911759638d2905203f384752
Zuber <zuberahmed99@gmail.com>
2017-08-03 20:24:02 +0530

    added k nearest neighbour

--- src/data/extractor/analysis.py
+++ src/data/extractor/analysis.py
@@ -5,8 +5,9 @@ import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 df = pd.read_csv("victim_info_2012_2017.csv")
 
@@ -36,7 +37,7 @@ def predict_random_forest(df):
     y = df['Race__White']
     print "splitting test and training data"
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=101, test_size=0.3)
-    rf = RandomForestClassifier(n_estimators=700)
+    rf = RandomForestClassifier(n_estimators=800)
     print "Fitting the training data into the random forest"
     rf.fit(X_train, y_train)
     print "Predicting values of test data"
@@ -44,4 +45,22 @@ def predict_random_forest(df):
     print "Printing classification report"
     print(classification_report(y_test, predictions))
 
-predict_random_forest(df)
+def predict_k_nearest_neighbours(df):
+    df = preprocessing.clean_data(df)
+    df = preprocessing.get_dummies(df)
+    # get X variables
+    X = df[['age', 'time', 'Cause__Stabbing', 'Cause__Assault', 'Cause__Auto Crash', 'Cause__Other', 'Cause__Shooting',
+            'Cause__Strangulation']]
+    y = df['Race__White']
+    print "splitting test and training data"
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=101, test_size=0.3)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, y_train)
+    print "Fitting the training data into k-nearest neighbour"
+    print "Predicting values of test data"
+    predictions = knn.predict(X_test)
+    print "Printing classification report"
+    print(classification_report(y_test, predictions))
+    print(confusion_matrix(y_test, predictions))
+
+predict_k_nearest_neighbours(df)
